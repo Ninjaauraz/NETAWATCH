70e9189713d8c1780099fb6c3255f2b7fab3bc13
Ninjaauraz <ambujankp@gmail.com>
2026-03-06 13:28:58 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import NetaWatchClient, { scoreBatch } from "../components/NetaWatchClient";
 import { SEED_POLITICIANS } from "../lib/seed";
 
 export default function Home() {
   const [pols] = useState(() => scoreBatch(SEED_POLITICIANS));
-  return <NetaWatchClient initialData={pols}/>;
+  return <NetaWatchClient initialData={pols} />;
 }
